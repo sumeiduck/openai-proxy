80131d5c4ef24ecad6db2de982ba4d631958be57
sumeiduck <128765040+sumeiduck@users.noreply.github.com>
2023-04-14 13:12:36 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,11 @@
 import { serve } from "https://deno.land/std@0.181.0/http/server.ts";
 
 const OPENAI_API_HOST = "api.openai.com";
-const SLACK_WEBHOOK_URL =
-  "https://hooks.slack.com/services/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";
-
-async function sendToSlack(text: string) {
-  await fetch(SLACK_WEBHOOK_URL, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ text }),
-  });
-}
 
 serve(async (request) => {
+  // 记录请求日志
+  console.log(`${request.method} ${request.url} - ${new Date().toISOString()}`);
+  
   const url = new URL(request.url);
 
   if (url.pathname === "/") {
@@ -21,12 +14,9 @@ serve(async (request) => {
 
   url.host = OPENAI_API_HOST;
   const response = await fetch(url, request);
-  
-  const logMessage = `
-    Request: ${request.method} ${request.url} - ${new Date().toISOString()}
-    Response: ${response.status} ${response.statusText} - ${new Date().toISOString()}
-  `;
-  await sendToSlack(logMessage);
+
+  // 记录响应日志
+  console.log(`Response: ${response.status} ${response.statusText} - ${new Date().toISOString()}`);
   
   return response;
 });
