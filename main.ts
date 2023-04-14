e257582d788cac3553b0cd03e3c2068a00a2fb63
sumeiduck <128765040+sumeiduck@users.noreply.github.com>
2023-04-14 13:55:18 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,16 @@ import { serve } from "https://deno.land/std@0.181.0/http/server.ts";
 
 const OPENAI_API_HOST = "api.openai.com";
 
+async function getLocalIP(): Promise<string> {
+  try {
+    const response = await fetch("https://ipinfo.io/ip");
+    const ip = await response.text();
+    return ip.trim();
+  } catch (error) {
+    return "Unknown IP";
+  }
+}
+
 serve(async (request) => {
   const url = new URL(request.url);
 
@@ -12,18 +22,20 @@ serve(async (request) => {
   url.host = OPENAI_API_HOST;
   const response = await fetch(url, request);
 
-  const ip = request.headers.get("X-Forwarded-For") || request.headers.get("x-real-ip") || "Unknown IP";
+  const ip = request.headers.get("X-Forwarded-For") || request.headers.get("X-Real-IP") || "Unknown IP";
+  const localIp = await getLocalIP();
   const headers = JSON.stringify(Object.fromEntries(request.headers.entries()));
   const params = url.search;
   const logMessage = `
-    Request:
+    Request -------------------------------------------
       Method: ${request.method}
       URL: ${request.url}
       Timestamp: ${new Date().toISOString()}
-      IP: ${ip}
+      Client IP: ${ip}
+      Local IP: ${localIp}
       Headers: ${headers}
       Params: ${params}
-    Response:
+    Response -------------------------------------------
       Status: ${response.status}
       Status Text: ${response.statusText}
       Timestamp: ${new Date().toISOString()}
