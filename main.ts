1891e085cd5948a43b9f1e7f30586cfe12880980
sumeiduck <128765040+sumeiduck@users.noreply.github.com>
2023-04-14 12:05:25 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -3,6 +3,9 @@ import { serve } from "https://deno.land/std@0.181.0/http/server.ts";
 const OPENAI_API_HOST = "api.openai.com";
 
 serve(async (request) => {
+  // 记录请求日志
+  console.log(`${request.method} ${request.url} - ${new Date().toISOString()}`);
+  
   const url = new URL(request.url);
 
   if (url.pathname === "/") {
@@ -10,5 +13,10 @@ serve(async (request) => {
   }
 
   url.host = OPENAI_API_HOST;
-  return await fetch(url, request);
+  const response = await fetch(url, request);
+
+  // 记录响应日志
+  console.log(`Response: ${response.status} ${response.statusText} - ${new Date().toISOString()}`);
+
+  return response;
 });
